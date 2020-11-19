0e74ade4741c0151ec486a568a734f7eaeba41da
dohyunKim12 <kdh951102@gmail.com>
2020-11-19 21:52:55 +0900

    weekdata추가

---
+++ app/src/main/java/com/examples/mobileProject/analysis/AnalysisDayData.java
@@ -0,0 +1,38 @@
+package com.examples.mobileProject.analysis;
+
+public class AnalysisDayData {
+    int date;
+    float pos;
+    float neg;
+
+
+    public int getDate() {
+        return date;
+    }
+
+    public void setDate(int date) {
+        this.date = date;
+    }
+
+    public float getPos() {
+        return pos;
+    }
+
+    public void setPos(float pos) {
+        this.pos = pos;
+    }
+
+    public float getNeg() {
+        return neg;
+    }
+
+    public void setNeg(float neg) {
+        this.neg = neg;
+    }
+
+    public AnalysisDayData(int date, float pos, float neg) {
+        this.date = date;
+        this.pos = pos;
+        this.neg = neg;
+    }
+}
